807f091704d74a25acdb1d39ea5d77ce98ec7e27
Peter Ottsen <peterottsen@Peters-MacBook-Pro-2.local>
2022-01-31 13:09:34 -0700

    test

--- postgres_app/config.py
+++ postgres_app/config.py
@@ -11,6 +11,7 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    # DATABASE_URL = 'postgresql://qvfvcvfuuvbxlv:9218667034f957f717746aec3050317d19156bcd4180221086103bf248cc1f71@ec2-18-235-154-252.compute-1.amazonaws.com:5432/d1ih6l1fogc1qu'
 
 
 class StagingConfig(Config):
